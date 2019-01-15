e08ae763686f0d4174a8be45914d4b95b0c7492e
Rafael <31940008+rdr42@users.noreply.github.com>
2019-01-14 15:22:52 -0900

    Created main.py file
    Here is the main.py file.

---
+++ main.py
@@ -0,0 +1,21 @@
+def parsing(input_):
+  alphabet_num = {0,1,2,3,4,5,6,7,8,9}
+  alphabet_op = {'+','*'}
+  alphabet_od = {'(',')',' '}
+
+  #Exceptions:
+    #Missing parantheses
+    #Numbers separated by space
+    #Operators followed by operators
+
+  #How to solve the calculation:
+    #Depth Counter to solve inner most operation
+    #Recursion to solve 
+  
+  #return value or exception
+
+#Take the input as a string
+var = input()
+
+#Calling function to execute program
+parsing(var)
