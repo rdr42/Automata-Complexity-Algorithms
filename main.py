fc5c97d0736ef83d26330e0bb5e863ac62a366bb
Devan Haynes <dhaynes12@alaska.edu>
2019-01-15 15:54:43 -0900

    Using and ast but not quite working

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-def parsing(input_):
-  alphabet_num = {0,1,2,3,4,5,6,7,8,9}
-  alphabet_op = {'+','*'}
-  alphabet_od = {'(',')',' '}
+import ast
+import operator
+
+alphabet_num = {0,1,2,3,4,5,6,7,8,9}
+alphabet_op = {'+','*'}
+alphabet_od = {'(',')',' '}
+
+OPERATORS = {ast.Add : operator.add,
+             ast.Mult : operator.mul}
+
+ #OPERATOR = {'+' : operator.add,
+ #           '*' : operator.mul}
+
+#Take the input as a string
+var = input()
+parsed_var = ' '.join(var.split())
+parsed_var = parsed_var.split(' ');
+
+
 
   #Exceptions:
     #Missing parantheses
@@ -14,8 +29,28 @@ def parsing(input_):
   
   #return value or exception
 
-#Take the input as a string
-var = input()
+
+class str_calc(ast.NodeVisitor):
+
+    def visit_op(self, node):
+        left_side = self.visit(node.left_side)
+        right_side = self.visit(node.right_side)
+        return OPERATORS[type(node.op)](left_side, right_side)
+
+    def visit_exp(self, node):
+        return self.visit(node.value)
+
+    def visit_number(self, node):
+        return node.n
+
+    @classmethod
+    def eval_expr(cls, expr):
+        tree = ast.parse(expr)
+        print (ast.dump(tree.body[0]))
+        calculating = cls()
+        return calculating.visit(tree.body[0])
+
+print (str_calc.eval_expr(var))
 
 #Calling function to execute program
-parsing(var)
+#parsing(var)
